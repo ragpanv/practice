370403398f09b489e1275e2de66a35ab9406810c
raginianvekar@gmail.com <raginianvekar@gmail.com>
2024-03-30 01:49:22 +0530

    one

--- first.py
+++ first.py
@@ -9,8 +9,5 @@ print('Welcome')
 print('adding new line after editing in growth branch, but not pulled')
 print('added line in growth branch')
 print('hmm')
-<<<<<<< HEAD
 print('diff change at local')
-=======
-print('diff change in git')
->>>>>>> de138dfdf3abc90571b9e0ca0f0d62b22f2be025
+
